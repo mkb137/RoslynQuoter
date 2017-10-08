fc4fc19cabfcd229920b640328380227d7b978c5
Mike Bennett <mkb137@gmail.com>
2017-10-08 14:15:15 -0600

    Deleted services, added paket, moved tests to external files

--- src/Quoter.Tests/Tests.cs
+++ src/Quoter.Tests/Tests.cs
@@ -1,6 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Xunit;
@@ -105,30 +104,6 @@ public class Tests
 .NormalizeWhitespace()", removeRedundantModifyingCalls: false, shortenCodeWithUsingStatic: true);
     }
 
-
-    [Fact]
-    public void TestGlobal()
-    {
-        Test(@"class C { void M() { global::System.String s; } }");
-    }
-
-    [Fact]
-    public void TestEmptyBlock()
-    {
-        Test(@"class C { void M() { } }");
-    }
-
-    [Fact]
-    public void TestInterpolatedString()
-    {
-        Test(@"class C { string s = $""a""; }");
-    }
-
-    [Fact]
-    public void TestAttribute()
-    {
-        Test(@"[Foo]class C { }");
-    }
 
     [Fact]
     public void TestHelloWorld()
@@ -242,331 +217,6 @@ namespace N
 .NormalizeWhitespace()", removeRedundantModifyingCalls: false);
     }
 
-    [Fact]
-    public void TestComment()
-    {
-        Test(@"class C
-{
-  void M()
-  {
-    A(""M""); // comment
-  }
-}");
-    }
-
-    [Fact]
-    public void TestSimpleStringLiteral()
-    {
-        Test("class C { string s = \"z\"; }"); // "z"
-    }
-
-    [Fact]
-    public void TestStringLiteralWithBackslash()
-    {
-        Test("class C { string s = \"a\\b\"");
-    }
-
-    [Fact]
-    public void TestSimpleIntLiteral()
-    {
-        Test("class C { int i = 42; }");
-    }
-
-    [Fact]
-    public void TestSimpleCharLiteral()
-    {
-        Test("class C { char c = 'z'; }");
-    }
-
-    [Fact]
-    public void TestTrueFalseAndNull()
-    {
-        Test("class C { var x = true ? false : null; }");
-    }
-
-    [Fact]
-    public void Roundtrip1()
-    {
-        Test("class C { string s = \"\\\"\"; }"); // "\""
-    }
-
-    [Fact]
-    public void Roundtrip2()
-    {
-        Test(@"using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        
-    }
-}");
-    }
-
-    [Fact]
-    public void Roundtrip3()
-    {
-        Test("class C { string s = \"\\\"\"; }");
-    }
-
-    [Fact]
-    public void Roundtrip4()
-    {
-        Test("class C { string s = @\" zzz \"\" zzz \"; }");
-    }
-
-    [Fact]
-    public void Roundtrip5()
-    {
-        Test(@"class C { void M() { M(1, 2); } }");
-    }
-
-    [Fact]
-    public void Roundtrip6()
-    {
-        Test(@"class C { bool b = true; }");
-    }
-
-    [Fact]
-    public void Roundtrip7()
-    {
-        Test(@"#error Foo");
-    }
-
-    [Fact]
-    public void Roundtrip8()
-    {
-        Test(@"#if false
-int i
-#endif");
-    }
-
-    [Fact]
-    public void Roundtrip9()
-    {
-        Test(@"\\\");
-    }
-
-    [Fact]
-    public void Roundtrip10()
-    {
-        Test(@"/// baz <summary>foo</summary> bar");
-    }
-
-    [Fact]
-    public void Roundtrip11()
-    {
-        Test(@"class /*///*/C");
-    }
-
-    [Fact]
-    public void Roundtrip12()
-    {
-        Test("#pragma checksum \"file.txt\" \"{00000000-0000-0000-0000-000000000000}\" \"2453\"");
-    }
-
-    [Fact]
-    public void Roundtrip13()
-    {
-        Test(@"class \\u0066 { }");
-    }
-
-    [Fact]
-    public void Roundtrip14()
-    {
-        Test(@"class C { }");
-    }
-
-    [Fact]
-    public void Roundtrip15()
-    {
-        Test(@"class C { void M() { ((Action)(async () =>
-                {
-                }))(); } }");
-    }
-
-    [Fact]
-    public void Roundtrip16()
-    {
-        Test(@"class C { void M() { a ? b : c; } }");
-    }
-
-    private static string GetPath(string relativePath)
-    {
-        if (Path.IsPathRooted(relativePath))
-        {
-            return Path.GetFullPath(relativePath);
-        }
-
-        return Path.GetFullPath(
-            Path.Combine(
-                Path.GetDirectoryName(
-                    Assembly.GetExecutingAssembly().Location),
-                relativePath));
-    }
-
-    private void RoundtripFile(string filePath)
-    {
-        Test(
-            File.ReadAllText(GetPath(filePath)),
-            useDefaultFormatting: false,
-            removeRedundantCalls: false,
-            shortenCodeWithUsingStatic: true);
-    }
-
-    [Fact]
-    public void Roundtrip20()
-    {
-        Test("#line 1 \"a\\b\"");
-    }
-
-    [Fact]
-    public void Roundtrip21()
-    {
-        Test("#line 1 \"a\\\b\"");
-    }
-
-    [Fact]
-    public void Roundtrip22()
-    {
-        Test("#pragma checksum \"..\\..\"");
-    }
-
-    [Fact]
-    public void Roundtrip23()
-    {
-        Test("class C { void P { a } }");
-    }
-
-    [Fact]
-    public void Roundtrip24()
-    {
-        Test(@"///
-class C { }");
-    }
-
-    [Fact]
-    public void Roundtrip25()
-    {
-        Test("class C { void M(__arglist) { M(__arglist); } }");
-    }
-
-    [Fact]
-    public void Roundtrip26()
-    {
-        Test(@"
-namespace @N
-{
-   public class @A
-   {
-       public @string @P { get; set; }
-   }
-}
-");
-    }
-
-    [Fact]
-    public void Roundtrip27()
-    {
-        Test("class C { void M() { int x; x = 42; } }");
-    }
-
-    [Fact]
-    public void Roundtrip28()
-    {
-        Test(@"[module: System.Copyright(""\n\t\u0123(C) \""2009"" + ""\u0123"")]");
-    }
-
-    [Fact]
-    public void SwitchCase()
-    {
-        Test(@"class C { public C() { switch(0) { case 1: break; default: break;} } } ");
-    }
-
-    [Fact]
-    public void TestObsoleteAttribute()
-    {
-        Test("class C { int i => 0; }");
-    }
-
-    [Fact]
-    public void TestNewlineInConstant()
-    {
-        Test(@"[module: System.Copyright(""\n"")]");
-    }
-
-    [Fact]
-    public void TestQuoteInLiteral()
-    {
-        Test(@"[module: A(""\"""")]");
-    }
-
-    [Fact]
-    public void TestQuoteInVerbatimLiteral()
-    {
-        Test(@"[module: A(@"""""""")]");
-    }
-
-    [Fact]
-    public void TestBackslashInLiteral()
-    {
-        Test(@"[module: A(""\\"")]");
-    }
-
-    [Fact]
-    public void RoundtripMissingToken()
-    {
-        Test("class");
-    }
-
-    [Fact]
-    public void TestXmlDocComment()
-    {
-        Test(@"    /// <summary>
-    /// test
-    /// </summary>
-class C { }");
-    }
-
-    [Fact]
-    public void TestXmlDocSummaryWithNamespace()
-    {
-        Test(@"    /// <summary xml:lang=""ru"">
-    /// test
-    /// </summary>
-class C { }");
-    }
-
-    [Fact]
-    public void TestXmlDocAll()
-    {
-        Test(@"/// <!--a-->
-/// <![CDATA[c]]>
-/// <completionlist cref=""a""/>
-/// <exception cref=""a"" />
-/// <include file='a' path='[@name=""b""]'/>
-/// <permission cref=""a"" />
-/// <remarks></remarks>
-/// <see cref=""a""/>
-/// <seealso cref=""a""/>
-/// <summary>a</summary>
-/// <example>a</example>
-/// <param name=""a"">a</param>
-class C { }");
-    }
-
-    [Fact]
-    public void TestDefaultLiteral()
-    {
-        Test(@"class C
-{
-    void A(int x = default)
-    {
-    }
-}");
-    }
-
     private void Test(
         string sourceText,
         string expected,
@@ -621,38 +271,32 @@ class C { }");
 
         Assert.Equal(sourceText, resultText);
     }
+	
+	[Theory]
+	[MemberData( nameof( GetTestFiles ), @"Resources\RoundTrip" ) ]
+	public void TestRoundtrip( string fileName ) {
+		// Read the file contents
+		using ( var stream = new FileStream( fileName, FileMode.Open, FileAccess.Read ) ) {
+			using ( var reader = new StreamReader( stream ) ) {
+				// Return the contents
+				var sourceText = reader.ReadToEnd();
+				Test( sourceText );
+			}
+		}
+	}
 
-    public void CheckSourceFiles()
-    {
-        var rootFolder = @"C:\roslyn-internal\Closed\Test\Files\";
-        var files = Directory.GetFiles(rootFolder, "*.cs", SearchOption.AllDirectories);
-        for (int i = 0; i < files.Length; i++)
-        {
-            VerifyRoundtrip(files[i]);
-        }
-    }
-
-    public void VerifyRoundtrip(string file)
-    {
-        try
-        {
-            var sourceText = File.ReadAllText(file);
-            if (sourceText.Length > 50000)
-            {
-                //Log("Skipped large file: " + file);
-                return;
-            }
-
-            Test(sourceText);
-        }
-        catch (Exception)
-        {
-            Log("Failed: " + file);
-        }
-    }
-
-    private static void Log(string text)
-    {
-        File.AppendAllText(@"Failed.txt", text + Environment.NewLine);
-    }
-}
+	/// <summary>
+	/// Returns the contents of every file in the given directory.
+	/// </summary>
+	/// <param name="directory"></param>
+	/// <returns></returns>
+	public static IEnumerable<object[]> GetTestFiles( string directory ) {
+		// Get the directory
+		var dirInfo = new DirectoryInfo( directory );
+		if ( !dirInfo.Exists ) throw new DirectoryNotFoundException( directory );
+		// For each file in the directory...
+		foreach ( var fileInfo in dirInfo.GetFiles() ) {
+			var filePath = Path.Combine( directory, fileInfo.Name );
+			yield return new [] { filePath };
+		}}
+	}
